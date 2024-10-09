bbad02525cc8807794000d3c841ce65f95d1aa88
Hideo Kawamura <hideo.90.kawamura@gmail.com>
2024-10-09 21:55:15 +0900

    change env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import starlight from '@astrojs/starlight';
 import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian';
 
 // import env
-const siteName = import.meta.env.CONFIG_SITE_NAME;
-const baseName = import.meta.env.CONFIG_BASE_NAME;
-const vaultName = import.meta.env.CONFIG_VAULT_NAME;
-const titleName = import.meta.env.CONFIG_TITLE_NAME;
-
+// const siteName = import.meta.env.CONFIG_SITE_NAME;
+// const baseName = import.meta.env.CONFIG_BASE_NAME;
+// const vaultName = import.meta.env.CONFIG_VAULT_NAME;
+// const titleName = import.meta.env.CONFIG_TITLE_NAME;
+const siteName = process.env.CONFIG_SITE_NAME || "";
+const baseName = process.env.CONFIG_BASE_NAME || "";
+const vaultName = process.env.CONFIG_VAULT_NAME || "VAULT";
+const titleName = process.env.CONFIG_TITLE_NAME || "TITLE";
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,11 +24,11 @@ export default defineConfig({
 				starlightObsidian({
 					vault: vaultName,
 				}),
-                        ],
+			],
 			title: titleName,
-                        // pagefindで日本語で検索できるようにするためlocaleを設定する
+			// pagefindで日本語で検索できるようにするためlocaleを設定する
 			locales: {
-                                 root: {
+				root: {
 					label: 'Japanese',
 					lang: 'ja',
 				},
