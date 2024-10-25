f16792302699dd31f84e65e7223f157129b3878d
Hideo Kawamura <hideo.90.kawamura@gmail.com>
2024-10-25 21:44:49 +0900

    Update astro.config.mjs
    typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
 				// Generate the Obsidian vault pages.
 				starlightObsidian({
 					vault: vaultName,
-					out: noteName
+					output: noteName
 				}),
 			],
 			title: titleName,
